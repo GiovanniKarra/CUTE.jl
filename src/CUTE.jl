64f94e897335c1c1a2022bc4406d803cf7c3639a
Giovanni Karra <karragiovanni@gmail.com>
2026-01-26 22:11:15 +0100

    improved error message

--- src/CUTE.jl
+++ src/CUTE.jl
@@ -78,6 +78,9 @@ function load_nl_file(path::AbstractString)::JuMP.Model
 end
 
 function get_model(name::AbstractString)::JuMP.Model
+	if !(name in COLLECTION[:, "Problem"])
+		throw(error("'$name' is not a valid CUTE problem name. For reference, visit https://arnold-neumaier.at/glopt/coconut/Benchmark/Library2_new_v1.html"))
+	end
 	try
 		load_nl_file(joinpath(@__DIR__, "..", "data", "NL", "$(name).nl"))
 	catch
